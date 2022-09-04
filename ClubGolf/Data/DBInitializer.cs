9fcdd8071c4338445be0036c22cfbc1a9442d019
Craig Griffiths <craigsaint@outlook.com>
2022-09-04 14:14:51 +0100

    Add project files.

---
+++ ClubGolf/Data/DBInitializer.cs
@@ -0,0 +1,62 @@
+﻿using ClubGolf.Models;
+
+namespace ClubGolf.Data
+{
+    public class DBInitializer
+    {
+        public static void Initialize(ClubContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (!context.Persons.Any())
+            {
+                var students = new Person[]
+                {
+                new Person{FirstName="Tom",LastName="Watson", Email="tomwat@golfmail.net",DateJoined=DateTime.Parse("2015-02-05")},
+                new Person{FirstName="Tiger",LastName="Woods", Email="twood@golfmail.net",DateJoined=DateTime.Parse("2012-03-21")},
+                new Person{FirstName="Bobby",LastName="Jones", Email="bobjones@golfmail.net",DateJoined=DateTime.Parse("2013-07-21")},
+                new Person{FirstName="Greg",LastName="Norman", Email="normalgreg@golfmail.net",DateJoined=DateTime.Parse("2012-09-11")}
+                };
+
+                foreach (Person c in students)
+                {
+                    context.Persons.Add(c);
+                }
+                context.SaveChanges();
+            }
+
+            if (!context.MembershipTypes.Any())
+            {
+                var membershiptypes = new MembershipType[]
+                {
+                new MembershipType{Description="Grand tour course A",AnnualCost=(decimal?)420.00},
+                new MembershipType{Description="Course B",AnnualCost=(decimal?)220.00},
+                new MembershipType{Description="Lounge and Bar",AnnualCost=(decimal?)120.00}
+                };
+
+                foreach (MembershipType c in membershiptypes)
+                {
+                    context.MembershipTypes.Add(c);
+                }
+                context.SaveChanges();
+            }
+
+            if (!context.Memberships.Any())
+            {
+                var memberships = new Membership[]
+                {
+                new Membership{PersonId=1,MembershipTypeId=1},
+                new Membership{PersonId=1,MembershipTypeId=3},
+                new Membership{PersonId=2,MembershipTypeId=2},
+                new Membership{PersonId=2,MembershipTypeId=3},
+                };
+
+                foreach (Membership c in memberships)
+                {
+                    context.Memberships.Add(c);
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ClubGolf/Models/Membership.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace ClubGolf.Models
+{
+    public class Membership 
+    {
+        [Key]
+        public int MembershipId { get; set; }
+
+        public int MembershipTypeId { get; set; }
+
+        public int PersonId { get; set; }
+
+        public bool MembershipActive { get; set; }
+
+        public MembershipType? MembershipType { get; set; }
+        public Person? Person { get; set; }
+
+    }
+
+
+
+}
